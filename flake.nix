91c03bbd96b8f996d89eebf86a7ba3d6315c880a
mr_screw <mr_screw@mail.ru>
2024-09-06 16:29:33 +0300

    .

--- flake.nix
+++ flake.nix
@@ -1,49 +1,52 @@
+Вот ваш код с добавленными краткими комментариями:
+
+```nix
 {
-  description = "My system configuration";
+  description = "My system configuration";  # Описание конфигурации системы
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";  # URL для unstable версии nixpkgs
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";  # URL для стабильной версии nixpkgs
+    home-manager = {  # Входные данные для Home Manager
+      url = "github:nix-community/home-manager";  # URL для Home Manager
+      inputs.nixpkgs.follows = "nixpkgs";  # следуйте за nixpkgs для версии
     };
   };
 
   outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }:
     let
-      system = "x86_64-linux";
-      pkgs= import nixpkgs {
+      system = "x86_64-linux";  # Определение системы
+      pkgs = import nixpkgs {    # Импорт пакетов с настройкой
         inherit system;
-        config.allowUnfree = true;
+        config.allowUnfree = true;  # Разрешение использования не свободных пакетов
       };
     in
     {
-      nixosConfigurations = {
-        nixos-master = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit pkgs; };
+      nixosConfigurations = {  # Определение NixOS конфигураций
+        nixos-master = nixpkgs.lib.nixosSystem {  # Конфигурация для nixos-master
+          system = "x86_64-linux";  # Система
+          specialArgs = { inherit pkgs; };  # Специальные аргументы (пакеты)
           modules = [
-            ./nixos/configuration.nix
+            ./nixos/configuration.nix  # Основной файл конфигурации
           ];
         };
-        nixos-mama = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit pkgs; };
+        nixos-mama = nixpkgs.lib.nixosSystem {  # Конфигурация для nixos-mama
+          system = "x86_64-linux";  # Система
+          specialArgs = { inherit pkgs; };  # Специальные аргументы (пакеты)
           modules = [
-            ./nixos/configuration-nixos-mama.nix
+            ./nixos/configuration.nix  # Основной файл конфигурации
           ];
         };
       };
 
-      homeConfigurations.master = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.${system};
-        modules = [ ./home-manager/home.nix ];
+      homeConfigurations.master = home-manager.lib.homeManagerConfiguration {  # Конфигурация для Home Manager
+        pkgs = nixpkgs.legacyPackages.${system};  # Пакеты для Home Manager
+        modules = [ ./home-manager/home.nix ];  # Основной файл конфигурации Home Manager
       };
 
-      #packages.${system} = {
-      #  nixos-mama = self.nixosConfigurations.nixos-mama.config.system.build.toplevel;
-      #  nixos-master = self.nixosConfigurations.nixos-master.config.system.build.toplevel;
-      #};
+      # packages.${system} = {  # Комментируемый код для сборки пакетов
+      #   nixos-mama = self.nixosConfigurations.nixos-mama.config.system.build.toplevel;  # Топ-уровневый набор для nixos-mama
+      #   nixos-master = self.nixosConfigurations.nixos-master.config.system.build.toplevel;  # Топ-уровневый набор для nixos-master
+      # };
     };
 }
--- nixos/packages.nix
+++ nixos/packages.nix
@@ -1,12 +1,10 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   nixpkgs.config = {
     allowUnfree = true;
   };
 
-  services.udev.packages = with pkgs; [
-    gnome.gnome-settings-daemon; # Дает возможность управления настройками GNOME на уровне системы
-  ];
-
+  services.gnome.gnome-settings-daemon.enable = true; # Дает возможность управления настройками GNOME на уровне системы
   services.openssh.enable = true; # Включаем OpenSSH
 
   programs.kdeconnect = {
@@ -25,7 +23,6 @@
     gnome-photos # Просмотр изображений
     gnome-terminal # Терминал GNOME
     gnome-tweaks # Настройка GNOME
-    gtk3-tools # Инструменты GTK 3 (возможно добавить, если требуется)
 
     # Мультимедиа
     ffmpeg # Мультимедийный фреймворк
@@ -45,7 +42,7 @@
     file # Утилита для работы с файлами
     git # Система управления версиями
     htop # Мониторинг процессов
-    nanon # Текстовый редактор (возможно добавить, если требуется)
+    nano # Текстовый редактор (возможно добавить, если требуется)
     ntfs3g # Поддержка NTFS
     scrot # Скриншотер
     unzip # Распаковка ZIP-архивов
@@ -55,8 +52,7 @@
     # Пакеты для разработки
     git # Система управления версиями
     nixpkgs-fmt # Форматирование Nix конфигураций
-    vscode-with-extensions.override
-    {
+    (vscode-with-extensions.override {
       vscodeExtensions = with vscode-extensions; [
         davidanson.vscode-markdownlint # Линтер для Markdown
         jnoortheen.nix-ide # Поддержка Nix в VS Code
@@ -68,32 +64,30 @@
       ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
         { name = "remote-ssh-edit"; publisher = "ms-vscode-remote"; version = "0.47.2"; sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g"; }
       ];
-    }
-
-    # Шрифты
-    fonts.packages = with pkgs; [
-    noto-fonts # Шрифты Noto
-    noto-fonts-emoji # Эмодзи шрифты Noto
-  ];
+    })
 
     # Темы и иконки
     numix-gtk-theme # GTK-тема Numix
     numix-icon-theme # Тема иконок Numix
     numix-icon-theme-circle # Круглая тема иконок Numix
     numix-cursor-theme # Тема курсора Numix
+
+    # Установка звуковых систем
+    pipewire # Звуковая система PipeWire
+    pulseaudio # Звуковая система PulseAudio
+
+    # Другие утилиты
+    home-manager # Управление конфигурацией
+
+    # Гном расширения   
+    gnomeExtensions.settingscenter # Центр настроек GNOME
+    gnomeExtensions.appindicator # Индикатор приложений GNOME
+    gnomeExtensions.gsconnect # GNOME версия KDE Connect
   ];
 
-  # Установка звуковых систем
-  environment.systemPackages ++ [
-  pipewire                    # Звуковая система PipeWire
-  pulseaudio                  # Звуковая система PulseAudio
+  # Шрифты
+  fonts.packages = with pkgs; [
+    noto-fonts # Шрифты Noto
+    noto-fonts-emoji # Эмодзи шрифты Noto
   ];
-
-  # Другие утилиты
-  home-manager                 # Управление конфигурацией
-
-  # Гном расширения   
-  gnomeExtensions.settingscenter  # Центр настроек GNOME
-  gnomeExtensions.appindicator     # Индикатор приложений GNOME
-  gnomeExtensions.gsconnect        # GNOME версия KDE Connect
-  ];
+}

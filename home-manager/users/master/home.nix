a2a79c11362323bb1d1dda155bff95a0082e37e5
mrscrew <mr_screw@mail.ru>
2024-09-09 07:51:10 +0300

    .

--- home-manager/users/master/home.nix
+++ home-manager/users/master/home.nix
@@ -1,12 +1,15 @@
 { pkgs, ... }: {
+  # Импорт конфигураций
   imports = [
-    ./zsh.nix
-    ./orca-slicer.nix
-    ../../modules/bundle.nix
+    ./zsh.nix          # Настройки Zsh
+    ./orca-slicer.nix   # Конфигурация OrcaSlicer
+    ../../modules/bundle.nix  # Дополнительные модули
   ];
+
+  # Основные настройки для пользователя
   home = {
-    username = "master";
-    homeDirectory = "/home/master";
-    stateVersion = "24.05";
+    username = "master";             # Имя пользователя
+    homeDirectory = "/home/master";  # Путь к домашней директории пользователя
+    stateVersion = "24.05";          # Версия состояния для обратной совместимости
   };
 }
--- home-manager/users/master/orca-slicer.nix
+++ home-manager/users/master/orca-slicer.nix
@@ -1,9 +1,12 @@
-{ pkgs, ... }:
-{
+{ pkgs, ... }: {
   home.packages = [
+    # Используем переопределённый пакет OrcaSlicer с поддержкой перевода
     pkgs.orca-slicer.overrideAttrs (oldAttrs: rec {
       postInstall = ''
+        # Скачивание файла перевода на русский язык
         wget https://raw.githubusercontent.com/SoftFever/OrcaSlicer/master/localization/i18n/OrcaSlicer_ru.po -O $out/share/OrcaSlicer/localization/i18n/OrcaSlicer_ru.po
+
+        # Конвертация файла перевода в бинарный формат .mo
         msgfmt $out/share/OrcaSlicer/localization/i18n/OrcaSlicer_ru.po -o $out/share/OrcaSlicer/resources/i18n/ru/OrcaSlicer.mo
       '';
     })

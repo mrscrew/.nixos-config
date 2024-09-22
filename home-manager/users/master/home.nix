a90c57dcf82a0b7ae21db9ed4c77ff9eb854dd1f
--global <mr_screw@mail.ru>
2024-09-23 02:20:09 +0300

    .

--- home-manager/users/master/home.nix
+++ home-manager/users/master/home.nix
@@ -1,21 +1,23 @@
 { pkgs, ... }: {
   # Импорт конфигураций
   imports = [
-    ./zsh.nix                 # Настройки Zsh
-    ../../modules/bundle.nix  # Дополнительные модули
+    ./zsh.nix # Настройки Zsh
+    ../../modules/bundle.nix # Дополнительные модули
+  ];
+
+  home.packages = with pkgs; [
+    prusa-slicer
   ];
 
   # Основные настройки для пользователя
   home = {
-    username = "master";             # Имя пользователя
-    homeDirectory = "/home/master";  # Путь к домашней директории пользователя
-    stateVersion = "24.05";          # Версия состояния для обратной совместимости
+    username = "master"; # Имя пользователя
+    homeDirectory = "/home/master"; # Путь к домашней директории пользователя
+    stateVersion = "24.05"; # Версия состояния для обратной совместимости
     file = {
-      "background.jpg".source = ./background.jpg;  # Добавление background.jpg в домашнюю директорию
-      "avatar.jpg".source = ./avatar.jpg;  # Добавление avatar.jpg в домашнюю директорию
+      "background.jpg".source = ./background.jpg; # Добавление background.jpg в домашнюю директорию
+      "avatar.jpg".source = ./avatar.jpg; # Добавление avatar.jpg в домашнюю директорию
+      ".nix-profile/share/PrusaSlicer/localization/be/PrusaSlicer.mo".source = "./PrusaSlicer.mo"
+        };
     };
-  };
-  home.packages = with pkgs; [
-    prusa-slicer
-  ];
-}
+  }

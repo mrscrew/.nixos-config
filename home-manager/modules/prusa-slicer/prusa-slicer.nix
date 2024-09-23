a692e961ebfe76a4f6e92068aef607597ba01ba2
mr_screw <mr_screw@mail.ru>
2024-09-23 16:51:11 +0300

    .

--- home-manager/modules/prusa-slicer/prusa-slicer.nix
+++ home-manager/modules/prusa-slicer/prusa-slicer.nix
@@ -1,25 +1,13 @@
 { pkgs, ... }:
-
-let
-  # Путь к файлу локализации
-  my-be-file = ./PrusaSlicer.mo;
-
-  # Оверлей для модификации пакета prusa-slicer
-  myOverlay = self: super: {
-    prusa-slicer = super.prusa-slicer.overrideAttrs (oldAttrs: {
-      postInstall = oldAttrs.postInstall + ''
-        mkdir -p $out/share/PrusaSlicer/localization/be
-        cp ${my-be-file} $out/share/PrusaSlicer/localization/be/
-      '';
-    });
-  };
-in
 {
   home.packages = with pkgs; [
-    prusa-slicer
+    prusa-slicer.overrideAttrs (prusaSlicerAttrs: {
+      # ... (другие настройки)
+      localization = {
+        be = {
+          PrusaSlicer.mo = "./PrusaSlicer.mo"; # Замените путь на реальный
+        };
+      };
+    })
   ];
-
-  # Подключение оверлея
-  nixpkgs.overlays = [ myOverlay ];
 }
-

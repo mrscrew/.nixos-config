045f7140daca1d8a05468ce8b2ef71d700398319
--global <mr_screw@mail.ru>
2024-09-29 20:45:21 +0300

    .

--- nixos/hosts/nixos-mama/configuration.nix
+++ nixos/hosts/nixos-mama/configuration.nix
@@ -49,8 +49,11 @@
 
   # Разрешение сборки пакетов с несвободными лицензиями
   nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "yandex-browser-stable"
+  ];
 
-  nixpkgs.config.permittedInsecurePackages = [ "yandex-browser-stable-24.7.3.1253-1" ];
+  #nixpkgs.config.permittedInsecurePackages = [ "yandex-browser-stable-24.7.3.1253-1" ];
 
   # Версия состояния системы, используемая для обратной совместимости
   system.stateVersion = "24.05";

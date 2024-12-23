bd01ab6b79f7be532f439cfcbf7de0e94fe5225a
--global <mr_screw@mail.ru>
2024-12-24 00:30:13 +0300

    .

--- home-manager/users/master/home.nix
+++ home-manager/users/master/home.nix
@@ -51,6 +51,7 @@
           { name = "remote-ssh-edit"; publisher = "ms-vscode-remote"; version = "0.47.2"; sha256 = "1hp6gjh4xp2m1xlm1jsdzxw9d8frkiidhph6nvl24d0h8z34w49g"; }
         ];
       })
+      arduino-ide
       dbeaver-bin
 
       # 3D-печать

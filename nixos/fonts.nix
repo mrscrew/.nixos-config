7609ec59351b87781c7b4e49916b9929ee8b9f06
mrscrew <mr_screw@mail.ru>
2026-09-01 00:07:46 +0000

    Update fonts.nix

--- nixos/fonts.nix
+++ nixos/fonts.nix
@@ -3,8 +3,8 @@
   # Базовые шрифты Noto, эмодзи и иконки Font Awesome
   
   fonts.packages = with pkgs; [
-    noto-fonts           # базовые шрифты Noto
-    noto-fonts-emoji     # эмодзи Noto
-    font-awesome         # иконки (для баров и уведомлений)
+    noto-fonts              # базовые шрифты Noto
+    noto-fonts-color-emoji  # эмодзи Noto
+    font-awesome            # иконки (для баров и уведомлений)
   ];
 }
